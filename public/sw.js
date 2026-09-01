fb07e2149f9bfe07913332cd95d71152bebc53f5
kat-anaqq <mdzhukow@gmail.com>
2026-09-01 15:19:49 +0300

    Обновлён кеш версии с анимациями

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vdvoem-shell-v5';
+const CACHE='vdvoem-shell-v6';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/app.html','/manifest.webmanifest','/favicon.svg','/apple-touch-icon.png','/icon-192.png','/icon-512.png']))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(event.request.method!=='GET'||url.pathname.startsWith('/api/'))return;if(['document','script','style','manifest','image'].includes(event.request.destination)){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))}});
